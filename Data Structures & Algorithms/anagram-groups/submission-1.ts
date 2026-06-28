8536fff7375c98fcfd10ec86855d5f63f58f5983
Momen Hussein <63496562+momen5406@users.noreply.github.com>
2026-06-28 22:53:14 +0300

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        const map = new Map<string, string[]>()        
+        for (const word of strs) {
+            const key = word.split("").sort().join("");
+
+            if (!map.has(key)) map.set(key, []);
+
+            map.get(key)!.push(word)
+        }
+
+        return Array.from(map.values());
+    }
+}
